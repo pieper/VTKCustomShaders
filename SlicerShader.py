a7b28fa70a10dabe9d29c21da37af454b1681ad7
Simon Drouin <drouin.simon@gmail.com>
2018-10-17 08:57:08 -0400

    Simplified the code by using new vtkUniforms class
    Now, we don't need to wait for the UpdateShader callback to set the fiducial position in the shader. It can be set directly in the mapper using the vtkUniforms instance of the mapper.

--- SlicerShader.py
+++ SlicerShader.py
@@ -7,35 +7,18 @@ renderWindow = layoutManager.threeDWidget(0).threeDView().renderWindow()
 renderer = renderWindow.GetRenderers().GetItemAsObject(0)
 mapper = renderer.GetVolumes().GetItemAsObject(0).GetMapper()
 
-@vtk.calldata_type(vtk.VTK_OBJECT)
-def shaderCallback(vtkObject, event, callData):
+def onFiducialMoved():
     p=[0]*3
     fn=slicer.util.getNode('vtkMRMLMarkupsFiducialNode1')
     fn.GetNthFiducialPosition(0,p)
-    callData.SetUniform3f( "fiducial", p )
-    slicer.modules.shaderProgram = callData
-
-mapper.AddObserver(vtk.vtkCommand.UpdateShaderEvent, shaderCallback)
-
-slicer.modules.shaderProgram = None
-def onFiducialMoved():
-    if slicer.modules.shaderProgram:
-        p=[0]*3
-        fn=slicer.util.getNode('vtkMRMLMarkupsFiducialNode1')
-        fn.GetNthFiducialPosition(0,p)
-        slicer.modules.shaderProgram.SetUniform3f( "fiducial", p )
-        mapper.Modified()
+    mapper.GetFragmentCustomUniforms().SetUniform3f( "fiducial", p )
+    mapper.Modified()
 
 fn=slicer.util.getNode('vtkMRMLMarkupsFiducialNode1')
 fn.AddObserver(fn.PointModifiedEvent, lambda caller,event: onFiducialMoved())
 
-mapper.AddShaderReplacement(
-        vtk.vtkShader.Fragment,
-        "//VTK::Cropping::Dec", True,
-        """
-            uniform vec3 fiducial;
-        """,
-        False)
+defaultFiducial=[0]*3
+mapper.GetFragmentCustomUniforms().AddUniform3f( "fiducial", defaultFiducial )
 
 croppingImplShaderCode = """
     vec4 texCoordRAS = in_volumeMatrix[0] * in_textureDatasetMatrix[0]  * vec4(g_dataPos, 1.);
